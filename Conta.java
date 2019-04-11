7b3b3583aed646459839d0056f92a3260889ea86
luanzincomp <49029493+luanzincomp@users.noreply.github.com>
2019-04-11 10:03:48 -0300

    Create Conta.java

---
+++ Conta.java
@@ -0,0 +1,60 @@
+package contaBancaria;
+
+public class Conta {
+	private String nomeCorretista;
+	private double saldo;
+	static final public double Default_taxaJuros = 0.1;
+	static public double taxa_Juros = Default_taxaJuros;
+	
+	public Conta(String nome,double saldo) throws Exception {
+		this.setConta(nome,saldo);
+	}
+	public Conta() throws Exception{
+		this("Padrao",0);
+	}
+	public double getSaldo() {
+		return this.saldo;
+	}
+	public String getNome() {
+		return this.nomeCorretista;
+	}
+	
+	public double getTaxa() {
+		return Conta.taxa_Juros;
+	}
+	public void setConta(String nome,double saldo) throws Exception{
+		if (saldo>=0) {
+			this.nomeCorretista = nome;
+			this.saldo = saldo;
+		}
+		else {
+			throw new Exception("Erro");
+		}
+	}
+	
+	public static void setJuros(double taxaJuros) {
+		Conta.taxa_Juros = taxaJuros;
+	}
+	public String toString () {
+		StringBuilder dados = new StringBuilder();
+		dados.append(getNome());
+		dados.append(";");
+		dados.append(getSaldo());
+		dados.append(";");
+		dados.append(getTaxa());
+		return dados.toString();
+	}
+	public boolean equals(Object Objeto) {
+		Conta aux = (Conta)Objeto;
+		if (this.getNome() == aux.getNome() && this.getSaldo() == aux.getSaldo()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static void corrigeSaldo(Conta P) {
+		double saldo = P.saldo;
+		P.saldo =saldo*taxa_Juros+ saldo;
+	}
+}
