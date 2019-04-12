d686a40564a622b197cf7f3bc705aee947dc584b
luanzincomp <49029493+luanzincomp@users.noreply.github.com>
2019-04-12 16:59:21 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -5,13 +5,14 @@ public class Conta {
 	private double saldo;
 	static final public double Default_taxaJuros = 0.1;
 	static public double taxa_Juros = Default_taxaJuros;
-	
+	//Construtores da classe
 	public Conta(String nome,double saldo) throws Exception {
 		this.setConta(nome,saldo);
 	}
 	public Conta() throws Exception{
 		this("Padrao",0);
 	}
+	//Métodos de interface gets
 	public double getSaldo() {
 		return this.saldo;
 	}
@@ -22,6 +23,7 @@ public class Conta {
 	public double getTaxa() {
 		return Conta.taxa_Juros;
 	}
+	//Métodos de interface sets
 	public void setConta(String nome,double saldo) throws Exception{
 		if (saldo>=0) {
 			this.nomeCorretista = nome;
@@ -35,6 +37,7 @@ public class Conta {
 	public static void setJuros(double taxaJuros) {
 		Conta.taxa_Juros = taxaJuros;
 	}
+	//Método para converter em uma string com formatação "nome;saldo;TaxaDejuros
 	public String toString () {
 		StringBuilder dados = new StringBuilder();
 		dados.append(getNome());
@@ -44,6 +47,7 @@ public class Conta {
 		dados.append(getTaxa());
 		return dados.toString();
 	}
+	//Método para verificar se dois objetos são iguais
 	public boolean equals(Object Objeto) {
 		Conta aux = (Conta)Objeto;
 		if (this.getNome() == aux.getNome() && this.getSaldo() == aux.getSaldo()) {
@@ -53,6 +57,7 @@ public class Conta {
 			return false;
 		}
 	}
+	//Método que modifica o valor do saldo em relação ao juros estabelecido
 	public static void corrigeSaldo(Conta P) {
 		double saldo = P.saldo;
 		P.saldo =saldo*taxa_Juros+ saldo;
